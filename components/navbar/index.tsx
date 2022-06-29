ae978c42786e888dcbacababa3d229fb1a8fdfad
Dominic-Wassef <domwassef@gmail.com>
2022-06-29 04:47:57 -0400

    adding

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -35,12 +35,12 @@ export default function Example() {
                 <div className="flex-shrink-0 flex items-center">
                   <img
                     className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    src="/images/page_logo.png"
                     alt="Workflow"
                   />
                   <img
                     className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                    src="/images/page_logo.png"
                     alt="Workflow"
                   />
                 </div>
